29c5e5a2ba1b8a3c5f0f9abbc97e86a7d5be1134
Your Name <you@example.com>
2024-09-17 17:19:05 +0300

    last commit

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/controller/cubit/posts_cubit.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 
 class Home extends StatelessWidget {
-  const Home({super.key});
-
+  const Home({super.key });
+  
   @override
   Widget build(BuildContext context) {
     return BlocConsumer<PostsCubit, PostsState>(
@@ -12,27 +13,44 @@ class Home extends StatelessWidget {
       builder: (context, state) {
         final cubit = context.read<PostsCubit>();
         return Scaffold(
-          body:  state is DataReach ?
-          ListView.builder(
-            itemCount: cubit.posts!.length  ,
-            itemBuilder: (context, index) {
-              return Card(
-                elevation: 1,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(19),
+          body: state is DataReach
+              ? ListView.builder(
+                  itemCount: cubit.posts!.length,
+                  itemBuilder: (context, index) {
+                    return Card(
+                      elevation: 2,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(19),
+                      ),
+                      color: Colors.amber,
+                      child: ListTile(
+                        title: Text(cubit.posts![index].title.toString()),
+                        subtitle: Text(cubit.posts![index].body.toString()),
+                        leading: Text(cubit.posts![index].id.toString()),
+                      ),
+                    );
+                  },
+                )
+              : Skeletonizer(
+                enabled: true,
+                  child: ListView.builder(
+                    itemCount: 12, 
+                    itemBuilder: (context, index) {
+                      return Card(
+                        elevation: 2,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(19),
+                        ),
+                        color: Colors.grey[300], 
+                        child: const ListTile(
+                          title: Text("dsdsdsddsdsdsdsdsdsdsd\ndsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsd"),
+                          subtitle: Text("dsdsddsdsdsdsdsds\ndsdsdsdsdsdsdsdsds\ndsdsdsdsdsds"),
+                          leading: Text("d"),
+                        ),
+                      );
+                    },
+                  ),
                 ),
-                color: Colors.amber,
-                child: ListTile(
-                  title: Text(cubit.posts![index].title.toString()),
-                  subtitle: Text(cubit.posts![index].body.toString()),
-                  leading: Text(cubit.posts![index].id.toString()),
-                ),
-              );
-            },
-            ):
-            Center(
-              child: CircularProgressIndicator(),
-            )
         );
       },
     );
